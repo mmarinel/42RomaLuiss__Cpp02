6b2bf9ace300bc5562778b76345f45b2dd34cc61
Matteo Marinelli <mmarinel@e4r7p4.42roma.it>
2022-09-09 15:33:56 +0200

    bsp: line corner case handled

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: mmarinel <mmarinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 14:46:48 by mmarinel          #+#    #+#             */
-/*   Updated: 2022/09/09 15:08:24 by mmarinel         ###   ########.fr       */
+/*   Updated: 2022/09/09 15:32:46 by mmarinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,24 +25,14 @@ bool	bsp( Point const a, Point const b, Point const c, Point const point )
 	Fixed	_YcMinusYa = c.getY() - a.getY();
 	Fixed	_XcMinusXa = c.getX() - a.getX();
 	Fixed	w1 =(
-		(
-			_YcMinusYa*(point.getX() - a.getX())
-			- (point.getY() - a.getY())*_XcMinusXa
-		)
+		(_YcMinusYa*(point.getX() - a.getX()) - (point.getY() - a.getY())*_XcMinusXa)
 		/
-		(
-			_YcMinusYa*(b.getX() - a.getX())
-			- (b.getY() - a.getY())*_XcMinusXa
-		)
+		(_YcMinusYa*(b.getX() - a.getX()) - (b.getY() - a.getY())*_XcMinusXa)
 	);
 	Fixed	w2 =(
-		(
-			point.getY() - a.getY() - w1 * (b.getY() - a.getY())
-		)
+		(point.getY() - a.getY() - w1 * (b.getY() - a.getY()))
 		/
-		(
-			_YcMinusYa
-		)
+		(_YcMinusYa)
 	);
 	return (
 		w1 > 0 &&
